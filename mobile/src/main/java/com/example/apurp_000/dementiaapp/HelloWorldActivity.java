ff87b01340bd9acd84f45599e6f4dbdb3a1efe4d
Andrew <apurpura@zagmail.gonzaga.edu>
2015-06-01 11:54:11 -0700

    Initial commit

---
+++ mobile/src/main/java/com/example/apurp_000/dementiaapp/HelloWorldActivity.java
@@ -0,0 +1,38 @@
+package com.example.apurp_000.dementiaapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+
+
+public class HelloWorldActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_world);
+
+        Button wearButton = (Button)findViewById(R.id.wearButton);
+        wearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int notificationId = 001;
+
+                NotificationCompat.Builder notificationBuilder =
+                        new NotificationCompat.Builder(HelloWorldActivity.this)
+                                .setSmallIcon(R.mipmap.ic_launcher)
+                                .setContentTitle("HelloWorld")
+                                .setContentText("You just got a notification bro!");
+
+                NotificationManagerCompat notificationManager =
+                        NotificationManagerCompat.from(HelloWorldActivity.this);
+
+                notificationManager.notify(notificationId, notificationBuilder.build());
+            }
+        });
+
+    }
+}
