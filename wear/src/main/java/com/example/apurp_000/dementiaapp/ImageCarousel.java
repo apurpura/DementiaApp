6127a166de3854f9be48b133921e5b51ba86c70f
rlthomas88 <rlthomas88@gmail.com>
2015-07-12 16:29:33 -0700

    Image Carousel Changes
    Images now scroll at 5 second intervals. Adjusted XML layout to show
    photo meta data under picture (WIP).

--- wear/src/main/java/com/example/apurp_000/dementiaapp/ImageCarousel.java
+++ wear/src/main/java/com/example/apurp_000/dementiaapp/ImageCarousel.java
@@ -1,80 +1,44 @@
 package com.example.apurp_000.dementiaapp;
 
         import android.app.Activity;
-        import android.content.Context;
-        import android.content.res.TypedArray;
         import android.os.Bundle;
-        import android.view.View;
-        import android.view.ViewGroup;
-        import android.widget.AdapterView;
-        import android.widget.AdapterView.OnItemClickListener;
-        import android.widget.BaseAdapter;
-        import android.widget.Gallery;
+        import android.os.Handler;
+        import android.widget.ImageSwitcher;
         import android.widget.ImageView;
-        import android.widget.Toast;
 
 /**
  * Created by Ryan on 6/24/2015.
  */
 
 public class ImageCarousel extends Activity {
-    //the brah to display
+    //Configure Array to Store Pictures.
     Integer[] imageIDs = {
             R.drawable.pic1,
             R.drawable.pic2,
             R.drawable.pic3,
             R.drawable.pic4
     };
+    int i = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_carousel);
 
-        // Note that Gallery view is deprecated in Android 4.1---
-        Gallery gallery = (Gallery) findViewById(R.id.gallery1);
-        gallery.setAdapter(new ImageAdapter(this));
-        gallery.setOnItemClickListener(new OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View v, int position,long id)
-            {
-                Toast.makeText(getBaseContext(),"pic" + (position + 1) + " selected",
-                        Toast.LENGTH_SHORT).show();
-                // display the brah selected
-                ImageView imageView = (ImageView) findViewById(R.id.image1);
-                imageView.setImageResource(imageIDs[position]);
+        final ImageView imageView = (ImageView) findViewById(R.id.image1);
+        //final Handler handler = new Handler();
+        Runnable r = new Runnable() {
+            public void run() {
+                imageView.setImageResource(imageIDs[i]);
+                i++;
+                if (i >= imageIDs.length) {
+                    i = 0;
+                }
+                imageView.postDelayed(this, 5000); //set to go off again in 3 seconds.
             }
-        });
-    }
-
-    public class ImageAdapter extends BaseAdapter {
-        private Context context;
-        private int itemBackground;
-        public ImageAdapter(Context c)
-        {
-            context = c;
-            // sets a grey background; wraps around the brah
-            TypedArray a =obtainStyledAttributes(R.styleable.MyGallery);
-            itemBackground = a.getResourceId(R.styleable.MyGallery_android_galleryItemBackground, 0);
-            a.recycle();
-        }
-        // returns the number of brah
-        public int getCount() {
-            return imageIDs.length;
-        }
-        // returns the ID of an item
-        public Object getItem(int position) {
-            return position;
-        }
-        // returns the ID of an item
-        public long getItemId(int position) {
-            return position;
-        }
-        // returns an ImageView view
-        public View getView(int position, View convertView, ViewGroup parent) {
-            ImageView imageView = new ImageView(context);
-            imageView.setImageResource(imageIDs[position]);
-            imageView.setLayoutParams(new Gallery.LayoutParams(100, 100));
-            imageView.setBackgroundResource(itemBackground);
-            return imageView;
-        }
+        };
+        imageView.postDelayed(r, 3000);
     }
 }
+
+
