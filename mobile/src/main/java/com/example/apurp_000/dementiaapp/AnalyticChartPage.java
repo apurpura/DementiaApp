60de40392e0c14a5eba33af43edf0e6ee290366f
Dinglitch <dinglitch@gmail.com>
2015-07-19 12:18:31 -0700

    fixed chart class to have overides
    made the chart class the correct activity

--- mobile/src/main/java/com/example/apurp_000/dementiaapp/AnalyticChartPage.java
+++ mobile/src/main/java/com/example/apurp_000/dementiaapp/AnalyticChartPage.java
@@ -17,10 +17,11 @@ import com.androidplot.xy.XYSeries;
 import com.androidplot.xy.*;
 import java.util.Arrays;
 
-public class AnalyticChartPage extends Activity {
+public class AnalyticChartPage extends AppCompatActivity {
 
     private XYPlot plot;
 
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         //get passed intent
         Intent intent = getIntent();
@@ -75,6 +76,27 @@ public class AnalyticChartPage extends Activity {
         plot.getGraphWidget().setDomainLabelOrientation(-45);
 
 
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_memory_recall_analytics, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
     public void closeChartListener(){
         final Button exitchart = (Button)findViewById(R.id.closechartbutton);
