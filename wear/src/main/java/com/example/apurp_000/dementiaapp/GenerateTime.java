1d1d12b1e49af1bfb48d0f574660ad9f578b0c8f
rlthomas88 <rlthomas88@gmail.com>
2015-07-18 20:29:40 -0700

    DateTime Format Change
    Changed the data/time format to something not so confusing for the
    Start/End/Cancel times of activities...Thanks Andy =)

--- wear/src/main/java/com/example/apurp_000/dementiaapp/GenerateTime.java
+++ wear/src/main/java/com/example/apurp_000/dementiaapp/GenerateTime.java
@@ -1,6 +1,8 @@
 package com.example.apurp_000.dementiaapp;
 
+import java.text.DateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by Ryan on 7/18/2015.
@@ -9,18 +11,9 @@ public class GenerateTime {
 
     //Generate the times for Analytics
     public String generateTimes ()
-    {   int zMinute;
-        int zHourOfDay;
-        String zMin;
-        String zHour;
-        String zTime;
-        final java.util.Calendar cal = java.util.Calendar.getInstance();
-        zMinute = cal.get(Calendar.MINUTE);
-        zHourOfDay = cal.get(Calendar.HOUR_OF_DAY);
-        zMin = Integer.toString(zMinute);
-        zHour = Integer.toString(zHourOfDay);
-        zTime = (zHour + ":" + zMin);
-        return zTime;
+    {
+        String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
+        return currentDateTimeString ;
     }
 
 }
