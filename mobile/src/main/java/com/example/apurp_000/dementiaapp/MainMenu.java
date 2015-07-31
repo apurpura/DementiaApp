19e72f1b5d946f3a4180d468bcb99250c1bc877b
Dinglitch <dinglitch@gmail.com>
2015-07-30 19:27:01 -0700

    channge to hamburger meni
    making a seperate class for menu pop up so setting things back

--- mobile/src/main/java/com/example/apurp_000/dementiaapp/MainMenu.java
+++ mobile/src/main/java/com/example/apurp_000/dementiaapp/MainMenu.java
@@ -3,8 +3,12 @@ package com.example.apurp_000.dementiaapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.PopupMenu;
+
+import com.google.android.gms.maps.MapsInitializer;
 
 /**
  * Created by Ryan on 6/21/2015.
@@ -22,6 +26,7 @@ public class MainMenu extends Activity {
         //listners for menu buttons
         setUpBtnlistners();
     }
+
     public void setUpBtnlistners(){
 
 
